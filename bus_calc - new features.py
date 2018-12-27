2c88264185b229842b016b5ab649cfbb59a2c084
TheLastEtlekmek <eklekmek07@gmail.com>
2018-12-27 18:29:08 +0300

    classier

--- bus_calc - new features.py
+++ bus_calc - new features.py
@@ -1,7 +1,5 @@
-#fix which b_lists error
 import time
 from math import floor
-#work in progres.. the program that shows how many minutes to next coming rk for Sarıtepe Campus
 
 rk_week = ("07.00", "08.00", "09.00", "09.55", "10.35", "12.15", "13.05", "13.50", "14.55", "15.35", "16.15", "17.30", "18.35", "19.15", "20.35", "21.15", "22.00", "23.55")
 rk_sat = ("07.20", "08.45", "10.00", "11.10", "12.25", "13.40", "15.00", "16.15", "17.45", "19.15", "20.35", "21.50", "23.55")
@@ -14,23 +12,24 @@ mekikA_end = ("10.00","16.00")
 mekikZ_week = ("13.00","14.30","19.00")
 mekikZ_end = ("11.00","13.30")
 
-class bus():
+class bus():    
     def __init__(self ,name ,week , sat, sun):
         self.name = name
         self.time_week = week
         self.time_sat = sat
         self.time_sun = sun
-        self.current_time = None
-        self.target_bus_time = None
+        self.current_time = 0
+        self.target_bus_time = 0
         self.current_day = time.strftime("%c")
-        self.remaning_time = None
+        self.remaining_time = 0
+        self.bus_count = 0
         if self.current_day == "Sat":
             self.used_list = self.time_sat
         elif self.current_day == "Sun":
             self.used_list = self.time_sun
         else:
             self.used_list = self.time_week
-
+    
     def update_time(self):
         cm = time.strftime("%M")
         ch = time.strftime("%H")
@@ -41,18 +40,18 @@ class bus():
 
     def closest_bus(self):
         self.update_time()
-        count = 0
         while True:
-            bh, bm = self.used_list[count].split(".")
+            bh, bm = self.used_list[self.bus_count].split(".")
             busL = (1971,0,0,int(bh),int(bm),0,0,0,0)
             busT = time.mktime(busL)
-            sub = busT - self.current_time
-            print(sub)
-            if sub > 0:
+            self.sub = busT - self.current_time
+            print(self.sub)
+            if self.sub > 0:
                 self.target_bus_time = busT
+                print("busT",busT)
                 break
-            count += 1
-            if len(self.used_list) == count:
+            self.bus_count += 1
+            if len(self.used_list) == self.bus_count:
                 if self.current_day == "Fri":
                     bh, bm = self.time_sat[0].split(".")
                     busL = (1971,0,1,int(bh),int(bm),0,0,0,0)
@@ -65,72 +64,28 @@ class bus():
                     bh, bm = self.time_week[0].split(".")
                     busL = (1971,0,1,int(bh),int(bm),0,0,0,0)
                     self.target_bus_time = time.mktime(busL)
+                break
 
-    def print_bus(self):
-        self.remaing_time = self.target_bus_time - self.current_time
-        print("target",self.target_bus_time)
-        print("kalan saniye",self.remaing_time)
     def bus_return(self):
-        self.remaing_time = self.target_bus_time - self.current_time
-        try:
-            return (self.remaning_time//3600,floor(self.remaning_time/60%60))
-        except TypeError:
-            return (floor(self.remaning_time/60%60))
-        
-#,floor(self.remaning_time/60%60)
+        self.remaining_time = self.target_bus_time - self.current_time
+        return round(self.remaining_time // 3600) , floor(self.remaining_time / 60 % 60)
+    
     def next_bus(self):
-        pass
-rk = bus("RK",rk_week,rk_sat,rk_sun)
-rk.closest_bus()
-def calc(bus = None):
-    cd = time.strftime("%c")
-    if cd == "Fri":
-        pass
-    elif cd == "Sat":
-        pass
-    elif cd == "Sun":
-        
-        pass
-    if bus == "rk":
-        if cd == "Sun":
-            b_list = rk_sun
-        elif cd == "Sat":
-            b_list = rk_sat
-        else:
-            b_list = rk_week
-    elif bus == "hs":
-        if cd == "Sun":
-            b_list = hs_sun
-        elif cd == "Sat":
-            b_list = hs_sat
-        else:
-            b_list = hs_week
-    elif bus == "mekikA":
-        if cd == "Sun" or "Sat":
-            b_list = mekikA_end
-        else:
-            b_list = mekikA_week
-    elif bus == "mekikZ":
-        if cd == "Sun" or "Sat":
-            b_list = mekikZ_end
-        else:
-            b_list = mekikZ_week
-
-    cm = time.strftime("%M")
-    ch = time.strftime("%H")
-    cs = time.strftime("%S") 
-    currentL = (1971,0,0,int(ch),int(cm),int(cs),0,0,0)
-    currentT = time.mktime(currentL)    
-    timedelta = 0 # bus time - current time
-    count = 0 #bus number from 0 to how many buses on that b_list
-    while timedelta <= 0: #I am trying to find when is the next bus
-        bh, bm = b_list[count].split(".")
+        self.update_time()
+        self.bus_count += 1
+        bh, bm = self.used_list[self.bus_count].split(".")
         busL = (1971,0,0,int(bh),int(bm),0,0,0,0)
         busT = time.mktime(busL)
-        sub = busT - currentT
-        if sub > 0:
-            break
-        count += 1
-        if len(b_list) == count:
-            return ("Next {} is tomorrow at {}".format(bus ,b_list[0]))
-    return ("Remaining to {}: {}.{}".format(bus,round(sub//3600),floor(sub/60%60)))
+        self.sub = busT - self.current_time
+        
+
+hs = bus("HS",hs_week,hs_sat,hs_sun)
+rk = bus("RK",rk_week,rk_sat,rk_sun)
+rk.closest_bus()
+print(rk.bus_return())
+def rk_calc(n = 0): #n as bus number
+    rk.closest_bus()
+    rk.bus_count += n
+    return rk.bus_return()
+print(rk_calc(1))
+
